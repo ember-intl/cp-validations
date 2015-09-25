605c6f7e9379c7931e721843a627c9bc648465a6
Jason Mitchell <jason.mitchell.w@gmail.com>
2015-09-24 23:41:07 -0700

    Initial commit

---
+++ tests/dummy/app/routes/application.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export default Ember.Route.extend({
+	intl: Ember.inject.service(),
+
+	beforeModel() {
+		this.get('intl').setLocale('en-us');
+	},
+
+	model() {
+		return this.container.lookup('model:dummy');
+	}
+});
